c2036b4bd58a738cebf6decc6ccd1679b77ba1ca
shong2430 <134196918+shong2430@users.noreply.github.com>
2025-05-07 17:09:16 +0800

    test layout

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,18 +1,32 @@
 // src/app/about/page.js
-'use client';  // 👈 標記整個頁面為 Client Component
+"use client"; // 👈 標記整個頁面為 Client Component
 
 import Button from "@/component/Button";
+import "./page.css";
+import { useEffect, useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
 
 export default function AboutPage() {
-    function test() {
-        console.log('aaaaaa');
+  const [query, setQuery] = useState("");
+  const debounceQuery = useDebounce(query, 500);
+  function test() {
+    console.log(query);
+  }
+  useEffect(() => {
+    if (debounceQuery) {
+      console.log(query);
     }
-
-    return (
-        <div className="p-10 text-center">
-            <h1 className="text-3xl font-bold title-bg">About Shong</h1>
-            <p className="mt-4 text-lg">This is the about page built with Next.js!</p>
-            <Button text="test" onClick={test} />
-        </div>
-    );
+  }, [debounceQuery]);
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold title-bg">About Shong</h1>
+      <p className="mt-4 text-lg test">
+        This is the about page built with Next.js!
+      </p>
+      <Button text="test" onClick={test}>
+        dsadsdd
+      </Button>
+      <input tpye="test" onChange={(e) => setQuery(e.target.value)} />
+    </div>
+  );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        dddya
         {children}
       </body>
     </html>
